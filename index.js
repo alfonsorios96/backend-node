f1badf5e6a49974beddd871bba3bf2ce91c66384
Alfonso Rios <malforime@gmail.com>
2018-02-12 15:03:23 -0600

    fix(mbfsta *): Changes QSRV_A02 -> SRVS_A02

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ app.get('/', (request, response) => {
 
 app.post('/QSRV_A02/TechArchitecture/mx/grantingTicket/V02', services.gtZonaPublica);
 app.post('/QSRV_A02/TechArchitecture/mx/grantingTicket/V02', services.gtZonaPrivada);
-app.post('/QSRV_A02/MBFSTA00/V01', services.mbfsta00);
-app.post('/QSRV_A02/MBFSTA01/V01', services.mbfsta01);
-app.post('/SRVS_A02/MBFSTA02/V01', services.mbfsta02);
-app.post('/QSRV_A02/MBFSTA03/V01', services.mbfsta03);
-app.post('/QSRV_A02/MBFSTA04/V01', services.mbfsta04);
 
+app.post('/SRVS_A02/MBFSTA00/V01', services.mbfsta00);
+app.post('/SRVS_A02/MBFSTA01/V01', services.mbfsta01);
+app.post('/SRVS_A02/MBFSTA02/V01', services.mbfsta02);
+app.post('/SRVS_A02/MBFSTA03/V01', services.mbfsta03);
+app.post('/SRVS_A02/MBFSTA04/V01', services.mbfsta04);
 
 app.post('/SRVS_A02/carInsurancePolicies/V01/listCoverages', services.listCoverages);
 app.post('/SRVS_A02/carInsurancePolicies/V01/listCarInsurancePolicies', services.listCarInsurancePolicies);
